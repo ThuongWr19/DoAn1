fbba2171f9247df26f3c09c08c8e2d0e147c85fd
ThuongWr19 <89179543+ThuongWr19@users.noreply.github.com>
2023-10-07 19:50:37 +0700

    Update thuvien.h

--- thuvien.h
+++ thuvien.h
@@ -11,38 +11,11 @@ typedef struct Student {
 	float subject[3]{}; //Math, English, c++
 	float averageSubject = 0;
 }STUDENT;
+
 typedef struct ContigousListStudent {
 	int n;
 	STUDENT list[MAX];
 }CLS;
-
-void OutputListStudent(CLS m);
-void OutputOneStudent(CLS m, int i);
-
-void EmptyListStudent(CLS& m);
-
-void deleteList(CLS &m);
-
-void averageSubject(STUDENT& student);
-
-void addNewStudent(CLS& m, int quantity);
-void removeStudent(CLS& m, int n);
-
-void searchByCode(CLS m, int code);
-void searchByName(CLS m, char name[]);
-
-void updateStudentInformation(STUDENT& sv);
-void updateStudent(CLS& m, int code);
-
-void splitDuplicateCode(CLS& m, CLS& m1);
-
-void removeDuplicateCode(CLS& m);
-
-bool joinAndSort(CLS& m, CLS m1, CLS m2);
-
-bool checkSumGoodStudentHeadHalfListIsBiggerThanSumGoodStudentFollowingHalfList(CLS m);
-
-void OutputBestStudent(CLS m);
 
 void ShowMenu();
 void menu(int n);
